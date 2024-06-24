03b7087ce69c8fee0e32c472fab67ce20b5e2a2e
Alexandre Uzan <alexandreuzan9@gmail.com>
2024-06-24 11:59:47 +0200

    ADD scripts Modify

---
+++ scripts/detect_objects.py
@@ -0,0 +1,4 @@
+from src.object_detection import detect_objects
+
+if __name__ == "__main__":
+    detect_objects()
---
+++ scripts/evaluate_model.py
@@ -0,0 +1,4 @@
+from src.evaluation import evaluate_model
+
+if __name__ == "__main__":
+    evaluate_model()
